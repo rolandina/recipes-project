499d56182ce09f8d9e108d8f7ca23bb9e81dd59f
Smirnova Nina <ms.nina.smirnova@gmail.com>
2021-07-29 11:21:05 +0200

    filled in classes Ingredient() and Recipe()

--- recipebook/ingredient.py
+++ recipebook/ingredient.py
@@ -1,6 +1,20 @@
-
+from recipebook.data import Data
 
 class Ingredient:
 
     def __init__(self):
-        self.get_with_popularities = 'a'
+        self.__data_dict = Data().get_data()
+        self.__ingredient_popularities = self.__create_popularities_table()
+
+    def __create_popularities_table(self):
+        
+        ingredients = self.__data_dict['ingredient']
+        items = self.__data_dict['recipe_items'] 
+
+        ingredient_popularities = ingredients.copy()
+        ingredient_popularities['polularity'] = items[['ingredient_id','recipe_id']].groupby(by = ['ingredient_id']).count()['recipe_id'].to_list()
+        ingredient_popularities['is_popular'] = [True if i >1 else False for i in ingredient_popularities['polularity']]
+        return ingredient_popularities
+
+    def get_with_popularities(self):
+        return self.__ingredient_popularities
--- recipebook/recipe.py
+++ recipebook/recipe.py
@@ -1,8 +1,30 @@
-
+from recipebook.data import Data
 
 class Recipe:
 
     def __init__(self):
         self.get_scored_recipes = []
+
+
+    def __init__(self):
+        self.__data_dict = Data().get_data()
+        self.__recipes_scored = self.__create_recipe_scored_table()
+
+    def __create_recipe_scored_table(self):
+        
+        recipes = self.__data_dict['recipe']
+        ingredients = self.__data_dict['ingredient']
+        items = self.__data_dict['recipe_items'] 
+
+        recipes_scored = recipes.copy()
+        joint1 = items[['ingredient_id', 'recipe_id']].join(ingredients[['Id', 'Score']].set_index('Id'), on="ingredient_id", how='left')
+        joint2 = joint1.join(recipes[['Id','name']].set_index('Id'), on = 'recipe_id', how = 'left')
+
+        recipes_scored['score'] = joint2[['name', 'Score']].groupby(by = ['name']).mean()['Score'].to_list()
+       
+        return recipes_scored[['name', 'score']]
+        
+    def get_scored_recipes(self):
+        return self.__recipes_scored
 
     
